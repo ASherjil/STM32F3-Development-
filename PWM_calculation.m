a85b2512ac8d4775296b8d6549ee85cfc836e3a2
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-01 06:29:51 +0000

    Added comments to make it more clear
    1- PSC is the prescaler
    2- Time is the inverse of the frequency of ISR required
    3-CCR is the value of register required for duty cycle

--- PWM_calculation.m
+++ PWM_calculation.m
@@ -1,13 +1,13 @@
 clear;clearvars;clc
 
+PSC = 3999
+time = 100^-1;% timer or freq required
+dutycycle = 25; % duty cycle in percentage
 
-Psc = 7999
-
-syms arr duty
-eqn = ((Psc+1)/(8*10^6))*(arr+1) == (100^-1);
-arr=double(solve(eqn,arr))
-
-% duty cycle in percentage
-dutycycle = 10; 
-eqn1 = (duty/(arr+1))*100 == dutycycle;
-duty = double(solve(eqn1,duty))
+%----------------------------------------------
+syms ARR CCR
+sysfreq = 8*10^6;% clock freq of the processor
+eqn = ((PSC+1)/(sysfreq))*(ARR+1) == time;
+ARR=double(solve(eqn,ARR))
+eqn1 = (CCR/(ARR+1))*100 == dutycycle;
+CCR = double(solve(eqn1,CCR))
