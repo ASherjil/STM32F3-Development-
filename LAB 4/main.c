9995629d4d19fa7d527baf2f1da58b9f7346cd92
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-10 10:03:24 +0000

    Update main.c

--- LAB 4/main.c
+++ LAB 4/main.c
@@ -1,10 +1,9 @@
-#include "stm32f3xx.h"                  // Device header
-#include <stdlib.h>
 #include "Throttle_Pedal.h"
 
 
 int main(void)
 {
-	DAC_init();
-
+	DAC_init(); // generate triangle wave at 1Hz frequency, PA.4 
+	encoder_generator(); // initialise the encoder emulator on PE.8 and PE.9
 }
+
