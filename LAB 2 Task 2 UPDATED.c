01faa42f75305916eb9c0656407a89b90acbefcf
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-10 16:14:17 +0000

    OP-Amp Improved

--- LAB 2 Task 2 UPDATED.c
+++ LAB 2 Task 2 UPDATED.c
@@ -4,6 +4,9 @@ static int count = 0;
 
 void wait(int a);
 void ADC_init();
+void DAC_init();
+void LEDS_ALL_init(); // initialise all GPIOE LEDs
+void TIM3_init();
 void OpAmp_init();
 
 void TIM3_IRQHandler()
@@ -21,32 +24,10 @@ void TIM3_IRQHandler()
 
 int main(void)
 {
-	
-// Enable clock on GPIO port E
-	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
-	
-// Configure ports for DAC
-	GPIOA->MODER |= 0x300; // PA.4 set to analogue mode 
-//-----------------ALL LED PINS SET TO PUSH/PULL WITH NO No pull-up, pull-down
-	GPIOE->MODER = (GPIOE->MODER & ~(0xFFFF0000))| 0x55550000;// output mode "01" for pins(8-15)
-	GPIOE->OTYPER &= ~(0xFF00); // push/pull "00" for pins(8-15)
-	GPIOE->PUPDR &= ~(0xFFFF0000); // no pullup, pull-down for pins(8-15)
-//--------------------------------------------------------------------------------
-	
-  RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Define the clock pulse toTIM3
-  TIM3->PSC = 65535;
-  TIM3->ARR = (int)11.2070;
-  TIM3->CR1 |= TIM_CR1_CEN;//Set Timer Control Register to start timer
-  TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an 'Update' Interrupt Enable (UIE) – or 0x00000001
-  NVIC_EnableIRQ(TIM3_IRQn); // Enable Timer 3 interrupt request in NVIC
-
-//-------------------------------------
-//DAC configuration 
-  RCC->APB1ENR |= RCC_APB1ENR_DAC1EN;
-  DAC1->CR |= DAC_CR_BOFF1;
-  DAC1->CR |= DAC_CR_EN1;
-//-------------------------------------
-
+		
+	LEDS_ALL_init();
+	TIM3_init();
+	DAC_init();
 	ADC_init();// initialise the ADC
 	OpAmp_init(); // initialise the OP-AMP
 		
@@ -58,10 +39,8 @@ int main(void)
 			while (!(ADC1->ISR & 0x4)) {}// wait for EOC flag to go high 
 			
 			GPIOE->BSRRH = 0xFF00; // turn OFF Leds 
-			GPIOE->BSRRL = (ADC1->DR) <<8; // turn ON Leds by shifting bits	
-						
+			GPIOE->BSRRL = (ADC1->DR) <<8; // turn ON Leds by shifting bits			
 	}
-	
 
 }
 
@@ -75,10 +54,46 @@ void wait(int c)
 	}
 }
 
+void LEDS_ALL_init() // initialise all GPIOE LEDs
+{
+	// Enable clock on GPIO port E
+	RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
+	//-----------------ALL LED PINS SET TO PUSH/PULL WITH NO No pull-up, pull-down
+	GPIOE->MODER = (GPIOE->MODER & ~(0xFFFF0000))| 0x55550000;// output mode "01" for pins(8-15)
+	GPIOE->OTYPER &= ~(0xFF00); // push/pull "00" for pins(8-15)
+	GPIOE->PUPDR &= ~(0xFFFF0000); // no pullup, pull-down for pins(8-15)
+//--------------------------------------------------------------------------------
+}
+
+void TIM3_init()
+{
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; //Define the clock pulse toTIM3
+  TIM3->PSC = 65535;
+  TIM3->ARR = (int)11.2070;
+  TIM3->CR1 |= TIM_CR1_CEN;//Set Timer Control Register to start timer
+  TIM3->DIER |= TIM_DIER_UIE; // Set DIER register to watch out for an 'Update' Interrupt Enable (UIE) – or 0x00000001
+  NVIC_EnableIRQ(TIM3_IRQn); // Enable Timer 3 interrupt request in NVIC
+}
+
+
+void DAC_init()
+{
+//DAC configuration 
+  RCC->APB1ENR |= RCC_APB1ENR_DAC1EN;
+	GPIOA->MODER |= 0x300; // PA.4 set to analogue mode 
+  DAC1->CR |= DAC_CR_BOFF1;
+  DAC1->CR |= DAC_CR_EN1;
+//-------------------------------------
+}
+
+
 void ADC_init()
 {
 	ADC1->CR &= ~(0x30000000); // 0b00 for the ADVREGEN = reset
 	ADC1->CR |= (0x10000000);// 0b01 for the ADVREGEN = enable 
+	
+	RCC->AHBENR |= RCC_AHBENR_GPIOFEN;// enable clock on GPIOF
+	GPIOF->MODER |= 0x30; // analogue mode on PF.2, "11" 
 		
 	wait(100);// 10us delay function 
 		
@@ -93,26 +108,20 @@ void ADC_init()
 	ADC1_2_COMMON->CCR |= 0x00010000;
 	//---------------------------------------------
 		
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;// enable clock on GPIOA
-	GPIOA->MODER |= 0x3; // analogue mode on PA.0, "11" 
-		
-	ADC1->CFGR |= (0x10);// 8 bit resolution 
+	ADC1->CR &= ~(0xC);// set ADSTART and JADSTART to 0, "00 00"	
+	
+	ADC1->CFGR = (ADC1->CFGR & ~0x18)|(0x10);// 8 bit resolution 
 	ADC1->CFGR &= ~(0x20);// align right 
 	ADC1->CFGR &= ~(0x2000);// no continous 
-		
-	//ADC1->SQR1 |= 0x40;// channel 1 in SQR1 
-	//ADC1->SQR1 &= ~(0x1);// only one conversion so length is 0
-	//ADC1->SMPR1 |= (0x18); // 7.5 ADC clock cycles
-		
-	ADC1->SQR1 = (ADC1->SQR1& ~0x7C0)|0x40;// channel 1 in SQR1, "000001" 
-	ADC1->SQR1 = (ADC1->SQR1& ~0xF)|0x1;// length of 1, "000001" 
-	ADC1->SMPR1 = (ADC1->SMPR1 & ~0x38)|0x18;	// 7.5 ADC clock cycles 
-		
+				
+	ADC1->SQR1 = (ADC1->SQR1& ~0x3C0)|0x280;// channel 10 in SQR1, "01010" 
+	ADC1->SQR1 &= ~(0xF);// only one conversion, L is "0000"
+	ADC1->SMPR1 |= 0x38; // 601.5 ADC clock cycles 
+	
 	ADC1->CR |= 0x1; // ADEN set to high to enable ADC
 	
 	while (!(ADC1->ISR & 0x01)) {}// wait for ARDY flag to go high 
 }
-
 void OpAmp_init()
 {
 //The OpAmp is connected to the system clock via the APB2 peripheral clock bus
@@ -128,5 +137,4 @@ void OpAmp_init()
 	OPAMP1->CSR |= 0x00000040;// Set VM_SEL to 0b10 to enable PGA mode
 	
 	OPAMP1->CSR = (OPAMP1->CSR & ~0x3C000)|0xC000;// gain set to 16 
-	
 }
