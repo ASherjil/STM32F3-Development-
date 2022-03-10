4dc9cf4a43bfecf21c8fc0634949816d4466e715
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-10 10:03:38 +0000

    Update Throttle_Pedal.h

--- LAB 4/Throttle_Pedal.h
+++ LAB 4/Throttle_Pedal.h
@@ -1,17 +1,25 @@
 #ifndef THROTTLE_PEDAL_H
 #define THROTTLE_PEDAL_H
 
-#include "stm32f3xx.h"                  // Device header
-#include <stdlib.h>
-
-#define PRESCALER1 255
-#define ARR1 121  // for 255 Hz frequency
+#define PRESCALER1 255 // for 255 Hz frequency, DAC/triangle wave 
+#define PRESCALER2 65535 // for 1 hz encoder 
+#define ARR1 121  
+#define ARR2 121
 #define PERIOD1 255
 #define MAX_AMPLITUDE 127
 
-static int count = 0;
+//-------------------------------------------------POTENTIOMETER INIT
 void DAC_init(void); // initialise the DAC with a timer-based interrupt
 void triangle_emulator(void); // write DAC values which output an analogue triangular signal 
+//-------------------------------------------------------------------
+
+
+
+//-------------------------------------------------ENCODER EMULATOR
+void encoder_signal(void); // emulate the encoder signal 
+void encoder_generator(void); // initialise the interrupt required to generate the encoder signal 
+//--------------------------------------------------------------------
+
 
 
 #endif
