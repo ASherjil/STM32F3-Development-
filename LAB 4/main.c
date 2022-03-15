c8a1d048bfdc1625f27c61c41e5a74c8eee6cad7
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-15 15:24:32 +0000

    Update main.c

--- LAB 4/main.c
+++ LAB 4/main.c
@@ -1,22 +1,22 @@
 #include "Throttle_Pedal.h"
 #include "stm32f3xx.h"                  // Device header
 
+
 int main(void)
 {
 	DAC_init(); // generate triangle wave at 1Hz frequency, PA.4 
 	encoder_generator(); // initialise the encoder emulator on PE.8 and PE.9
-	OpAmp_init();
-	ADC_init();
-	interrupt_init();
+	OpAmp_init(); // PA.5 as input, PA.2 as output 
+	ADC_init();// PF.2 as input 
+	interrupt_init();// button interrupt, PA.0
+	CountLEDs_init();
+	ext_interrupt1_init(); // init interrupt on PA.1
+	ext_interrupt2_init();// init interrupt on PC.3
+
 	
-	
-	while (1)
+	while(1)
 	{
-			ADC1->CR |= 0x4; // enable ADC
-			while (!(ADC1->ISR & 0x4)) {}// wait for EOC flag to go high
-			GPIOE->BSRRH = (0xF800); // turn OFF Leds
-			writeLEDs();
+		writeLEDs();
 	}
-	
-	
+
 }
