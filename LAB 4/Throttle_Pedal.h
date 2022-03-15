9d3c03ea23c0f82859e26b58b316b9e1f4d5f291
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-15 12:38:54 +0000

    Update Throttle_Pedal.h

--- LAB 4/Throttle_Pedal.h
+++ LAB 4/Throttle_Pedal.h
@@ -16,24 +16,24 @@ void triangle_emulator(void); // write DAC values which output an analogue trian
 
 //-------------------------------------------------ENCODER EMULATOR
 void encoder_signal(void); // emulate the encoder signal 
-void encoder_generator(void); // initialise the interrupt required to generate the encoder signal 
+void encoder_generator(void); // initialise the interrupt required to generate the encoder signal
+void ext_interrupt1_init(void); // initialise interrupt pins, PA.1
+void ext_interrupt2_init(void);// initialise interrupt pins , PC.2
 //--------------------------------------------------------------------
 
 //-------------------------------------------------Op-Amp and ADC INIT
-void OpAmp_init(void);
-void ADC_init(void);
+void OpAmp_init(void); // PA.5 as input, PA.2 as output 
+void ADC_init(void);// PF.2 as input 
 void wait(int);
 //--------------------------------------------------------------------
 
 //--------------------------------------Button Interrupt PA.0, EXTI0 and STATES 
-void interrupt_init(void);
+void interrupt_init(void);// button interrupt, PA.0
 void test_options(void);
 enum tests{POTENTIOMETER,ENCODER,COMBINED_TEST};
-void CountLEDs_init(void);
+void CountLEDs_init(void); // Initialise LED PE.11-15
 void writeLEDs(void);
 //-----------------------------------------------------------------------------
-
-
 
 
 #endif
