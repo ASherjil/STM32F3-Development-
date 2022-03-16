cc876ea048bb26c9b41f1e8d8fdf90f4b2a1418b
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-16 12:12:02 +0000

    Update main.c

--- LAB 4/main.c
+++ LAB 4/main.c
@@ -5,14 +5,13 @@
 int main(void)
 {
 	DAC_init(); // generate triangle wave at 1Hz frequency, PA.4 
+	ext_interrupt1_init(); // init interrupt on PA.1
+	ext_interrupt2_init();// init interrupt on PC.3
 	encoder_generator(); // initialise the encoder emulator on PE.8 and PE.9
 	OpAmp_init(); // PA.5 as input, PA.2 as output 
 	ADC_init();// PF.2 as input 
 	interrupt_init();// button interrupt, PA.0
 	CountLEDs_init();// init LED PE11-15 
-	ext_interrupt1_init(); // init interrupt on PA.1
-	ext_interrupt2_init();// init interrupt on PC.3
-
 	
 	while(1)
 	{
