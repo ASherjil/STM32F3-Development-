e41e9252a94a95583f77e9f079d07365589da5f6
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-18 15:37:17 +0000

    Added code for calculating the resolution

--- PSC and ARR Calc.m
+++ PSC and ARR Calc.m
@@ -2,7 +2,7 @@ clear;clearvars;clc
 
 ARR = 3999
 time = 1/100 ;% timer or freq required
-dutycycle = 25; % duty cycle in percentage
+dutycycle = 75; % duty cycle in percentage
 
 %----------------------------------------------
 syms PSC CCR
@@ -11,3 +11,4 @@ eqn = ((PSC+1)/(sysfreq))*(ARR+1) == time;
 PSC=double(solve(eqn,PSC))
 eqn1 = (CCR/(ARR+1))*100 == dutycycle;
 CCR = double(solve(eqn1,CCR))
+resolution = log(ARR+1)/log(2)
