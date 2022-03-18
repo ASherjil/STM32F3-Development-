d77bf7c37cd3b08dc2219c17d74e32701a022191
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-18 15:35:31 +0000

    Swapped around the ARR

--- PSC and ARR Calc.m
+++ PSC and ARR Calc.m
@@ -1,13 +1,13 @@
 clear;clearvars;clc
 
-PSC = 3999
-time = 100^-1;% timer or freq required
+ARR = 3999
+time = 1/100 ;% timer or freq required
 dutycycle = 25; % duty cycle in percentage
 
 %----------------------------------------------
-syms ARR CCR
+syms PSC CCR
 sysfreq = 8*10^6;% clock freq of the processor
 eqn = ((PSC+1)/(sysfreq))*(ARR+1) == time;
-ARR=double(solve(eqn,ARR))
+PSC=double(solve(eqn,PSC))
 eqn1 = (CCR/(ARR+1))*100 == dutycycle;
 CCR = double(solve(eqn1,CCR))
