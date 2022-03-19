f8d7a00f80a1983968f9ed40671de0e44ac95e19
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-19 07:47:36 +0000

    Added loop for all CCR/duty Cycles

--- PSC and ARR Calc.m
+++ PSC and ARR Calc.m
@@ -2,13 +2,23 @@ clear;clearvars;clc
 
 ARR = 3999
 time = 1/100 ;% timer or freq required
-dutycycle = 75; % duty cycle in percentage
+dutyCycles=[0 10 25 50 75 90 100];
 
-%----------------------------------------------
-syms PSC CCR
+
+
+%---------------------------------------------
+resolution = log(ARR+1)/log(2)
+syms PSC
 sysfreq = 8*10^6;% clock freq of the processor
 eqn = ((PSC+1)/(sysfreq))*(ARR+1) == time;
 PSC=double(solve(eqn,PSC))
-eqn1 = (CCR/(ARR+1))*100 == dutycycle;
-CCR = double(solve(eqn1,CCR))
-resolution = log(ARR+1)/log(2)
+
+formatSpec = 'Duty cycle %d= CCR of %f';
+
+for i = 1:length(dutyCycles)
+syms CCR
+duty = dutyCycles(i);
+eqn1 = (CCR/(ARR+1))*100 == duty;
+CCR = double((solve(eqn1,CCR)));
+str= sprintf(formatSpec,duty,CCR)
+end
