4de57fc0504a5b6767bcad672c81538d19db03fd
areebTP <92602684+areebTP@users.noreply.github.com>
2022-03-19 07:54:02 +0000

    Update PSC and ARR Calc.m

--- PSC and ARR Calc.m
+++ PSC and ARR Calc.m
@@ -1,13 +1,14 @@
 clear;clearvars;clc
 
-ARR = 3999
+ARR = 7999
 time = 1/100 ;% timer or freq required
+% percentage of duty cycles
 dutyCycles=[0 10 25 50 75 90 100];
 
 
-
 %---------------------------------------------
-resolution = log(ARR+1)/log(2)
+resolution = log(ARR+1)/log(2);
+PWM_res = sprintf('Resolution:%f bits',resolution)
 syms PSC
 sysfreq = 8*10^6;% clock freq of the processor
 eqn = ((PSC+1)/(sysfreq))*(ARR+1) == time;
@@ -20,5 +21,5 @@ syms CCR
 duty = dutyCycles(i);
 eqn1 = (CCR/(ARR+1))*100 == duty;
 CCR = double((solve(eqn1,CCR)));
-str= sprintf(formatSpec,duty,CCR)
+CCR_values= sprintf(formatSpec,duty,CCR)
 end
